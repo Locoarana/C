de5da37bd44fa929da2e2eeac4a0c45f46cc64f8
Locoarana <sebastian.arana2016@gmail.com>
2020-06-08 22:08:54 -0500

    primer commit

---
+++ chau.cpp
@@ -0,0 +1,11 @@
+//despedirme
+//
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"chau";
+	return 0;
+
+}
+
---
+++ hello.c
@@ -0,0 +1,10 @@
+//Escribir hello world
+//
+#include<stdio.h>
+using namespace std;
+int main(){
+
+	cout<<"Hello World";
+	return 0;
+
+}
